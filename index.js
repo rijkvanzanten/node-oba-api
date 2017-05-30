07974e01aa4d2be6ec3a6b7a565b45e85d86100d
Rijk van Zanten <rijkvanzanten@me.com>
2017-05-30 17:57:47 +0200

    :sparkles: add json class wrapper

--- index.js
+++ index.js
@@ -1,7 +1,48 @@
+const hmacsha1 = require('hmacsha1');
+const queryString = require('query-string');
+const parser = require('xml2json');
+const axios = require('axios');
+const btoa = require('btoa');
+
 class OBA {
-  constructor(keys) {
-    console.log(keys);
+  constructor(options) {
+    this.publicKey = options.public;
+    this.secretKey = options.secret;
+  }
+
+  get(endpoint, params) {
+    return new Promise((resolve, reject) => {
+      const baseUrl = 'http://obaliquid.staging.aquabrowser.nl/api/v1/';
+
+      const nl = '\n';
+
+      const acceptHeader = 'application/xml';
+      const date = new Date().toUTCString();
+      const hostHeader = 'obaliquid.staging.aquabrowser.nl';
+      const path = endpoint + '/';
+      const sortedQuery = queryString.stringify(params);
+
+      const id = acceptHeader + nl + date + nl + hostHeader + nl + path + nl + sortedQuery + nl;
+
+      const hash = hmacsha1(this.secretKey, id);
+
+      const result = btoa(hash);
+
+      axios.get(baseUrl + path, {
+        params,
+        headers: {
+          'X-AquaBrowser-Date': date,
+          Authorization: `AquaBrowser: ${this.publicKey};${result}`,
+          Host: 'obaliquid.staging.aquabrowser.nl'
+        }
+      })
+        .then(res => res.data)
+        .then(xml => parser.toJson(xml))
+        .then(res => resolve(res))
+        .catch(err => reject(err));
+    });
   }
 }
 
 module.exports = OBA;
+
